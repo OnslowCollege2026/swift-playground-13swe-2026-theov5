b55c58f894a53b90640fa7afa4fea4bf7da54998
theov5 <142852664+theov5@users.noreply.github.com>
2026-02-10 23:59:49 +0000

    Started Score Task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,15 +1,58 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+let passingGrade = 50
+
 @main
 struct SwiftPlayground {
     static func main() {
-        let words = ["apple", "cat", "banana", "dog", "grape", "kiwi"]
+        
+        var numbers = [1, 2, 3, 4, 5]
+        // Creates a list containing cubed numbers.
+        let cubedNumbers = numbers.map { number in
+            return number * number * number
+        }
+        
+        print(cubedNumbers)
+        
+        let words = ["apple", "cat", "banana", "dog", "grape", "kiwi", "strawberry"]
         print(words)
 
-        let shortenedWords = words.filter {word in 
-        return word.count <= 4}
+        // Removes any words which are less than 5 characters.
+        let lengthenedWords = words.filter {word in 
+        return word.count >= 4}
 
-        print(shortenedWords)
+        print(lengthenedWords)
+
+        numbers = [7, 14, 21, 28, 35]
+        print(numbers)
+
+        // Creates a new list containing all the numbers added together.
+        let addedNumbers = numbers.reduce(0) {result, number in
+        return result + number}
+        
+        print(addedNumbers)
+
+        // Returns the longest word in lengthened words.
+        let longestWord = lengthenedWords.reduce("") {result, word in
+        
+        // If the result is longer than the current word returns result.
+        if result.count > word.count {
+            return result
+        }
+        // If not returns word.
+        return word
+        }
+
+        print (longestWord)
+
+
+        let scores = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
+
+        let averageScore = scores.map{
+            return $0 + 5
+        }.filter{
+            return $0 >= passingGrade
+        }.reduce
     }
 }
