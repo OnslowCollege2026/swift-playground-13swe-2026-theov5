9ee59e6acf89ac46288510e485ff8f83013fef2b
theov5 <142852664+theov5@users.noreply.github.com>
2026-02-12 22:11:33 +0000

    Finished Task A

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,32 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let mixed = ["cat", "7", "owl", "15", "dog", "3"]
+
+        // Creates a list containing the numbers from mixed converted to integers.
+        let numbers = mixed.compactMap {Int($0)}
+        print(numbers)
+        
+        /// Checks if all the items are integers by converting them to an integer and checking if it returned nil.
+        /// If it returned nil it means the item was a string, and if it doesn't it means it was an integer.
+        let areAllNumbers = mixed.allSatisfy {Int($0) != nil}
+
+        // If mixed is all numbers prints a message (this will never trigger).
+        if areAllNumbers {
+            print("Yeehaw, I love numbers!")
+        }
+
+        let archive = [
+            [
+                [["candle", "dust"], ["mirror", "ash"]],
+                [["whisper", "shadow"], ["clock", "veil"]]
+            ],
+            [
+                [["stone", "key"], ["relic", "name"]],
+                [["cipher", "bone"], ["ember", "seal"]]
+            ]
+        ]
+
+        let chosenWord = archive.last
     }
 }
